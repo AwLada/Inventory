ff97ef56fff02527bc4016fc24018e017e546ad4
awlada <awlada@mail.csuchico.edu>
2019-12-02 20:35:53 -0800

    test

--- main.cpp
+++ main.cpp
@@ -7,20 +7,34 @@ using namespace std;
 
 int main()
 {
-    string input;
-    vector <string> inventory;
+        string input;
+        vector <string> inventory;
+        string word = "";
+        int a = 0;
 
-    cout<<"What's in the store (separate items by commas)? ";
-    getline(cin,input);
+        cout<<"What's in the store (separate items by commas)? ";
+        getline(cin,input);
 
-    inventory = listFromString(input); //TODO: Write this function!
+        for(int i=0; i<input.size(); i++)
+        {
+                if (!input[i] == ',')
+                {       a++;
+                        word.append (a,input[i]);
 
-    cout<<inventory.size()<<" items in inventory.\n";
-    for(int i=0; i<inventory.size(); i++)
-    {
-        cout<<inventory.at(i)<<endl;
-    }
+                }
+                else
+                {
+                        inventory.push_back (word);
+                        string word ="";
+                        a = 0;
+                }
+        }
+        cout<<inventory.size()<<" items in inventory.\n";
+        for(int i=0; i<inventory.size(); i++)
+        {
+                cout<<inventory[i]<<endl;
+        }
 
-    return 0;
+        return 0;
 }
-           
+
